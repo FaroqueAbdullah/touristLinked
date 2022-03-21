668887ab53566019af9b94d747aecc5baf62482e
Faroque Abdullah <abdullahabir3@gmail.com>
2022-03-22 02:33:32 +0600

    initial express folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('starting')
+import express, { Request, Response } from 'express';
+
+const PORT = process.env.SERVER_PORT || 4000;
+
+const app = express()
+
+app.get('/' , (req: Request, res: Response) => {
+  res.send('hello')
+})
+
+app.listen(PORT, () => {
+  console.log('server has started sadf')
+})

a7b03c7b5f57959f0cbef7b679bd05a63a3617d3
Faroque Abdullah <abdullahabir3@gmail.com>
2022-03-25 01:59:19 +0600

    react suspense added

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import { AppContainerStyled, BodyContainerStyled } from './components/StyledComponents/StyledComponents'
 import HeaderLayout from './layout/Header/Header';
 import FooterLayout from './layout/Footer/Footer';
-import HomeCompoment from './pages/Home/Home';
-import ProfileCompoment from './pages/Profile/Profile';
-import AuthComponent from './pages/Auth/Auth';
+
+const HomeCompoment = lazy(() => import('./pages/Home/Home'));
+const ProfileCompoment = lazy(() => import('./pages/Profile/Profile'));
+const AuthComponent = lazy(() => import('./pages/Auth/Auth'));
 
 
 function App() {
@@ -14,13 +15,15 @@ function App() {
     <AppContainerStyled>
       <HeaderLayout />
       <BodyContainerStyled>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomeCompoment />} />
-            <Route path="profile/" element={<ProfileCompoment />} />
-            <Route path='AuthComponent/' element={<AuthComponent />} />
-          </Routes>
-        </BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomeCompoment />} />
+              <Route path="profile/" element={<ProfileCompoment />} />
+              <Route path='AuthComponent/' element={<AuthComponent />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </BodyContainerStyled>
       <FooterLayout />
     </AppContainerStyled>

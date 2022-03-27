7c447ab39df69dd3de514ecbd23143b697251b6b
Faroque Abdullah <abdullahabir3@gmail.com>
2022-03-28 03:26:24 +0600

    swagger added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import 'dotenv/config';
 
 import { authRouter } from './routes/authentication/auth.router';
 import { articleRouter } from './routes/article/article.router';
+
+import * as swaggerdoc from './utils/swagger.json';
 
 
 import { connect } from './config/db.js';
 
 const PORT = process.env.SERVER_PORT || 4000;
 
+const options: swaggerJSDoc.Options = swaggerdoc;
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
 connect();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(bodyParser.json());
 app.use('/auth' , authRouter);
 app.use('/article' , articleRouter);

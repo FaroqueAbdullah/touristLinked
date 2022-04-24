546dcc7b2874bdf5d82f4313b866964bbc5a80a3
Faroque Abdullah <abdullahabir3@gmail.com>
2022-04-25 02:45:21 +0600

    profile and event

--- client-react/src/layout/EventSection/EventSection.tsx
+++ client-react/src/layout/EventSection/EventSection.tsx
@@ -1,7 +1,42 @@
 function EventSectionCompoment() {
   return (
 
-      <div className="h-full bg-white-primary rounded-lg">EventSectionCompoment</div>
+      <div className="h-full bg-white-primary rounded-lg p-3">
+
+
+        <div>
+          <div className="font-bold w-100">Popular Events</div>
+          <div className="w-full flex justify-between border-2 border-white-secondary mt-2 rounded-lg p-2">
+            <div>
+              Dhaka Tour
+            </div>
+            <div>
+              12/12/22
+            </div>
+          </div>
+          <div className="w-full flex justify-between border-2 border-white-secondary mt-2 rounded-lg p-2">
+            <div>
+              India Tour
+            </div>
+            <div>
+              12/12/22
+            </div>
+          </div>
+          <div className="w-full flex justify-between border-2 border-white-secondary mt-2 rounded-lg p-2">
+            <div>
+              London Tour
+            </div>
+            <div>
+              12/12/22
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+      </div>
     
   );
 }
--- client-react/src/layout/ProfileSection/ProfileSection.tsx
+++ client-react/src/layout/ProfileSection/ProfileSection.tsx
@@ -1,7 +1,18 @@
+import { FaUser } from "react-icons/fa";
+
 function ProfileSectionCompoment() {
   return (
     
-      <div className="h-full bg-white-primary rounded-lg">ProfileSectionCompoment</div>
+      <div className="h-auto bg-white-primary rounded-lg p-3 flex flex-col items-center">
+        <div className="border-2 rounded-full m-3 p-1 w-20 h-20 flex justify-center items-center text-5xl">
+          <FaUser />
+        </div>
+
+        <div className="w-full flex"> <div className="font-bold pr-2">Total Country Visited: </div> 20</div>
+        <div className="w-full flex"> <div className="font-bold pr-2">Total City Visited: </div> 84</div>
+        <div className="w-full flex"> <div className="font-bold pr-2">Upcoming Event Date: </div> 2/2/22</div>
+        <div className="w-full flex"> <div className="font-bold pr-2">Upcoming Event Place: </div> India</div>
+      </div>
     
   );
 }

91570f3cda3306ef9128c4da22b8d92705bb64fa
Faroque Abdullah <abdullahabir3@gmail.com>
2023-10-05 00:05:39 +0600

    [FE] - zustand added

---
+++ client-react/src/store/userStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+type State = {
+  email: string,
+  firstName: string,
+  lastName: string
+}
+
+type Action = {
+  setUser: (userData: State) => void
+  removeUser: (userData: State) => void
+}
+
+
+const useStore = create<State & Action>((set) => ({
+  email: '',
+  firstName: '',
+  lastName: '',
+  setUser: (userData) => set(() => ({ 
+    email: userData.email, 
+    firstName: userData.firstName, 
+    lastName: userData.lastName 
+  })),
+  removeUser: () => set(() => ({ 
+    email: '', 
+    firstName: '', 
+    lastName: '' 
+  })),
+}))
+
+export default useStore;

61d3af7f23dfff7edeb4ba36e358a17164194df1
Faroque Abdullah <abdullahabir3@gmail.com>
2023-10-15 22:29:43 +0600

    [BE] - test code updated

--- server/src/__test__/auth.test.ts
+++ server/src/__test__/auth.test.ts
@@ -2,12 +2,17 @@ import request from 'supertest';
 
 import app from '../app';
 
-it('return 200 OK when signup request is valid', () => {
-  request(app)
-    .post('api/auth/login')
-    .send({
-      email: 'abdullahabir3+1@gmail.com',
-      password: 'Abir@1234',
-    })
-    .expect(200);
+describe('User authentication', () => {
+  it('return 200 OK when signup request is valid', (done) => {
+    request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'abdullahabir3+1@gmail.com',
+        password: 'Abir@1234',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
 });

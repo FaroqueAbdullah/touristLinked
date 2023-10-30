679f12c308b98a0171f1f0bdfa4d387f543d3b3e
Faroque Abdullah <abdullahabir3@gmail.com>
2023-10-30 10:17:32 +0600

    [FE] error boundary added

--- client-react/src/layout/MainLayout.tsx
+++ client-react/src/layout/MainLayout.tsx
@@ -1,25 +1,25 @@
 import NavBar from '@/components/molecules/Navbar';
+import FallbackRender from '@/components/templates/FallBackError';
 import { Box } from '@mui/material';
-import Container, { ContainerProps } from '@mui/material/Container';
-import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
 import { Suspense } from 'react';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
 interface LayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const MainLayout = ({children}: LayoutProps): React.ReactNode => (
+const MainLayout = ({ children }: LayoutProps): React.ReactNode => (
   <>
     <NavBar />
-    <Box sx={{ padding: "6rem 0 1rem" }}>
-      <Container maxWidth="xl">
-      <Suspense fallback={<div>Loading...</div>}>
-        { children }
-      </Suspense>
-      </Container>
+    <Box sx={{ padding: '6rem 0 1rem' }}>
+      <ErrorBoundary fallbackRender={FallbackRender}>
+        <Container maxWidth="xl">
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </Container>
+      </ErrorBoundary>
     </Box>
   </>
-    
-)
+);
 
-export default MainLayout
+export default MainLayout;
